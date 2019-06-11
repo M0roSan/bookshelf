60f36aeb87b7553a1a8065d20bf512e3ea71f29e
Masa Yoshida <masa.yoshida@toyotaconnected.com>
2019-06-11 09:11:12 -0500

    Add to dev

--- app.py
+++ app.py
@@ -1,11 +1,19 @@
 from flask import Flask
 
 
-app = Flask(__name__)
+app = Flask(__name__, template_folder='templates')
 
 @app.route('/')
 def hello():
     return 'Hello world'
+
+@app.route('/hi/<name>')
+def say_hi(name):
+    return f'Hello, {name}!'
+
+@app.route('/age/<int:age>')
+def show_age(age):
+    return f'Age is {age}'
 
 
 if __name__ == '__main__':
